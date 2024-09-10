aa121e9eb7b2579d35e84e0ca5145d6c401d0224
Higor Carvalho <higorcarvalho502@gmail.com>
2024-09-10 18:27:15 -0300

    atv de script

---
+++ atividades da facul/contagem.js
@@ -0,0 +1,5 @@
+let Inicio = parseInt(prompt('Me diga um número inteiro para iniciar a contagem: '))
+let fim = parseInt(prompt('Me diga um número inteiro para terminar a contagem: '))
+for (Inicio; fim ; fim--){
+    alert(fim);
+}
