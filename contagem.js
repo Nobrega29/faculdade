29041a9e2bf625a295b621c0c7883c514fff1b4d
Higor Carvalho <higorcarvalho502@gmail.com>
2024-09-10 18:19:34 -0300

    Delete contagem.js

--- contagem.js
+++
@@ -1,5 +0,0 @@
-let Inicio = parseInt(prompt('Me diga um número inteiro para iniciar a contagem: '))
-let fim = parseInt(prompt('Me diga um número inteiro para terminar a contagem: '))
-for (Inicio; fim ; fim--){
-    alert(fim);
-}
