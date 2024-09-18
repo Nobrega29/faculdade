48b2aa5d5a729d6c652d9ca5b539b52d404dab54
Higor Carvalho <higorcarvalho502@gmail.com>
2024-09-18 12:32:18 -0300

    Create media.js

---
+++ atividades da facul/script/media.js
@@ -0,0 +1,13 @@
+let n1 = parseFloat(prompt('Me diga uma nota: '))
+let n2 = parseFloat(prompt('Me diga uma nota: '))
+let n3 = parseFloat(prompt('Me diga uma nota: '))
+let n4 = parseFloat(prompt('Me diga uma nota: '))
+let media 
+media = (n1 + n2 + n3 + n4)/4
+if ( media >= 7){
+    alert('sua nota é:' + media)
+    alert('Aprovado')
+} else{
+    alert('sua nota é:' + media)
+    alert('Reprovado')
+}
